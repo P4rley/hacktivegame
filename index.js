adab9f9d0f6f77ad736c9679f1567bcc98181ef0
P4rley <adwihersa@gmail.com>
2022-08-18 21:19:15 +0700

    First Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let sumbitBtn = document.getElementById("submit");
+
+sumbitBtn.onclick = function () {
+  let nama = document.getElementById("nama").value;
+  document.getElementById("namaValue").innerHTML = nama;
+
+  let usia = document.getElementById("usia").value;
+  document.getElementById("usiaValue").innerHTML = usia;
+
+  let lomba = document.getElementById("pilih-permainan").value;
+  document.getElementById("lombaValue").innerHTML = lomba;
+
+  let gender = document.getElementById("jenis-kelamin").value;
+  document.getElementById("genderValue").innerHTML = gender;
+
+  let berat = document.getElementById("weight").value;
+  document.getElementById("beratValue").innerHTML = berat;
+
+  let announce = document.getElementById("announce");
+
+  if (announce.className == "hide") {
+    announce.className = "show";
+  }
+};
