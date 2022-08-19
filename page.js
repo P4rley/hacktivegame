068487baaaa83925948383e1338dc235c531a8d2
P4rley <adwihersa@gmail.com>
2022-08-19 09:35:41 +0700

    Done

--- page.js
+++ page.js
@@ -27,28 +27,33 @@ sumbitBtn.onclick = function (e) {
 
     document.getElementById("cta").appendChild(newP);
 
+    let el = document.getElementById("audioNotifikasi");
+    el.volume = 0.1;
+
     let newSubmit = document.createElement("button");
     newSubmit.innerText = "Send";
     newSubmit.className = "ubah-btn";
+    newSubmit.id = "send";
     document.getElementById("cta").appendChild(newSubmit);
 
     let newButton = document.createElement("button");
     newButton.innerText = "Ubah Pilihan";
     newButton.className = "ubah-btn";
+    newButton.id = "delete-btn";
     document.getElementById("cta").appendChild(newButton);
 
     newSubmit.onclick = function () {
       let announce = document.getElementById("announce");
+      el.play();
 
       if (announce.className == "hide") {
         announce.className = "show";
       }
-
-      newButton.remove();
     };
 
     newButton.onclick = function () {
       window.location.reload();
+      el.pause();
     };
   }
 };
